6b43a21b170a477dee22ce95e011dff7861f510d
Wyatt Johnson <wyattjoh@gmail.com>
2012-09-19 12:21:18 -0600

    Added proper stuct and myPow function

--- Encrypt/Encrypt.ino
+++ Encrypt/Encrypt.ino
@@ -1,17 +1,67 @@
+/*
+  Step 1: Press button to indicate ready
+
+
+*/
+#include "structs.h"
+
+pub_t pub;
+pri_t pri;
+
+uint32_t myPow(uint32_t b, uint32_t p)
+{
+  uint32_t x = 1;
+  
+  for(int i = 0; i < p; i++)
+  {
+    x = b * x;
+  }
+    
+  return x;
+}
 
 void setup()
 {
   // Ini ports
-  
+ 
   Serial.begin(9600);
   Serial3.begin(9600);
   
+  pub.p = 37;
+  pub.g = 5;
+  
+  pri.a = 12;
+  pub.A = myPow(pub.g,pri.a) % pub.p;
 }
 
 int16_t key = 100;
 
 void loop()
 {
+  Serial.print("Public P: ");
+  Serial.print(pub.p, HEX);
+  Serial.print(" - ");
+  Serial.print(pub.p, DEC);
+  Serial.print(" - ");
+  Serial.println(pub.p, BIN);
+  
+  Serial.print("Public G: ");
+  Serial.print(pub.g, HEX);
+  Serial.print(" - ");
+  Serial.print(pub.g, DEC);
+  Serial.print(" - ");
+  Serial.println(pub.g, BIN);
+  
+  Serial.print("Shared key: ");
+  Serial.print(pub.A, HEX);
+  Serial.print(" - ");
+  Serial.print(pub.A, DEC);
+  Serial.print(" - ");
+  Serial.println(pub.A, BIN);
+  
+  Serial.print("\n");
+  delay(1000);
+  
   // read from port 3
   if(Serial3.available())
   {
---
+++ Encrypt/structs.h
@@ -0,0 +1,22 @@
+#ifndef structures_h
+#define structures_h
+
+#include <WString.h>
+
+typedef struct
+{
+  int32_t a;
+  int32_t s;
+}
+pri_t;
+
+typedef struct
+{
+  int32_t p;
+  int32_t g;
+  int32_t A;
+  int32_t B;
+}
+pub_t;
+
+#endif
